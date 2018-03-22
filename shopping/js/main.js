7d57b2d0d82e72733da375ae19d763ef5cf35483
WindSoar <sdust.wj@foxmail.com>
2018-03-22 11:41:48 +0800

    添加js

--- shopping/js/main.js
+++ shopping/js/main.js
@@ -4,6 +4,21 @@ $(document).ready(function (){
 		$('#cssfile').attr('href','styles/skin/'+skinName+'.css');
 		$.cookie('MyCssSkin',skinName,{path: '/',expires:10});
 	}
+	function showImg(index){
+		var $rollobj = $('#jnImageroll');
+		var $rolllist = $rollobj.find('div a');
+		var newhref = $rolllist.eq(index).attr('href');
+		$('#JS_imgWrap').attr('href', newhref).find("img").eq(index).stop(true, true).fadeIn().siblings().fadeOut();
+		$rolllist.removeClass('chos').css('opacity', '0.7').eq(index).addClass('chos').css('opacity', '1');
+	}
+	function showBrandList(index){
+		var $rollobj = $('#jnBrandList');
+		var rollWidth = $rollobj.find('li').outerWidth();
+		rollWidth = rollWidth * 4;
+		$rollobj.stop(true, false).animate({
+			left : -rollWidth *index
+		}, 1000);
+	}
 	// 搜索框
 	(function (){
 		$("#inputSearch").focus(function (){
@@ -37,6 +52,90 @@ $(document).ready(function (){
 
 	// 导航效果
 	(function (){
+		$('#nav li').hover(function (){
+			$(this).find('.jnNav').show();
+		},function (){
+			$(this).find('.jnNav').hide();
+		});
+	})();
 
-	}());
+	//左侧商品分类热销效果
+	(function (){
+		$('.jnCatainfo .promoted').append('<span class="hot"></span>');
+	})();
+
+	//右侧上部产品广告效果
+	(function (){
+		var index = 0;
+		var $imgrolls = $('#jnImageroll div a');
+		var len = $imgrolls.length;
+		var adTimer = null;
+		$imgrolls.css('opacity', '0.7');
+		$('#jnImageroll div a').mouseover(function (){
+			index = $('#jnImageroll div a').index(this);
+			showImg(index);
+		}).eq(0).mouseover();
+		$('#jnImageroll').hover(function (){
+			if(adTimer){
+				clearInterval(adTimer);
+			}
+		}, function (){
+			adTimer = setInterval(function (){
+				showImg(index);
+				index++;
+				if(index == len){
+					index = 0;
+				}
+			}, 5000);
+		}).trigger('mouseleave');
+	})();
+
+	//右侧最新动态模块内容添加超链接提示
+	(function (){
+		var x = 10;  
+		var y = 20;
+		$("a.tooltip").mouseover(function(e){
+			this.myTitle = this.title;
+			this.title = "";	
+			var tooltip = "<div id='tooltip'>"+ this.myTitle +"</div>";
+			$("body").append(tooltip);
+			$("#tooltip").css({
+				"top": (e.pageY+y) + "px",
+				"left": (e.pageX+x)  + "px"
+			}).show("fast");
+		}).mouseout(function(){		
+			this.title = this.myTitle;
+			$("#tooltip").remove();
+		}).mousemove(function(e){
+			$("#tooltip").css({
+				"top": (e.pageY+y) + "px",
+				"left": (e.pageX+x) + "px"
+			});
+		});
+	})();
+
+	//右侧下部光标划过产品列表效果
+	(function (){
+		$('#jnBrandTab li a').click(function (){
+			$(this).parent().addClass('chos').siblings().removeClass('chos');
+			var idx=$('#jnBrandTab li a').index(this);
+			showBrandList(idx);
+			return false;
+		}).eq(0).click();
+	})();
+
+	//右侧下部光标划过产品列表效果
+	(function (){
+		$('#jnBrandList li').each(function (index){
+			var $img = $(this).find("img");
+			var img_w = $img.width();
+			var img_h = $img.height();
+			var spanHtml = '<span style="position:absolute;top:0;left:5px;width:'+img_w+'px;height:'+img_h+'px;" class="imageMask"></span>';
+			$(spanHtml).appendTo(this);
+		});
+		$("#jnBrandList").delegate(".imageMask", "hover", function(){
+			alert();
+			$(this).toggleClass("imageOver");
+		});
+	})();
 });
